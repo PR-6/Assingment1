9e1f41f99c32e90ca26f1abed663ae10ce1d566e
PR-6 <78289888+PR-6@users.noreply.github.com>
2021-01-31 00:14:43 -0600

    Add files via upload

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public InputField playerName;
+    public static string username;
+
+    public void StartGame()
+    {
+        Debug.Log("Player Name is: " + playerName.text);
+        username = playerName.text;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public static string userName()
+    {
+        return username;
+    }
+}
